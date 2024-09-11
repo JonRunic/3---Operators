8881b96441089296e826e0d1ab12acf689ff80a8
Jonathan Williams <epicboijojo@gmail.com>
2024-09-11 14:04:31 -0400

    Initial Commit

---
+++ Lab - Operators.playground/Pages/6. App Exercise - Complex Fitness Calculations.xcplaygroundpage/Contents.swift
@@ -0,0 +1,13 @@
+
+
+/*:
+ One feature you might want to give users is to display their current body temperature. Create a constant `tempInFahrenheit` equal to 98.6. 
+ 
+ You may want to also show the temperature in celsius. You can convert fahrenheit to celsius by taking `tempInFahrenheit` and subtracting 32, then multiplying the result by (5.0/9.0). Create a constant `tempInCelsius` that calculates in one line the temperature in celsius.
+ */
+let tempInFahrenheit = 98.6
+let tempInCelsius = (tempInFahrenheit - 32) * (5.0/9.0)
+print(tempInCelsius)
+/*:
+[Previous](@previous)  |  page 6 of 8  |  [Next: Exercise - Numeric Type Conversion](@next)
+ */
